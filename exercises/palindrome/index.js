2b1daf49b270dadbc5ef958f9b0aaf1a2ca87c05
Pavan <pavantejakusunuri@gmail.com>
2021-02-02 21:31:46 +0530

    Added palindrome next method in code

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,8 +8,13 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    const reversed = str.split('').reverse().join('');
-    return str === reversed;
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    })
 }
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+//     return str === reversed;
+// }
 
 module.exports = palindrome;
